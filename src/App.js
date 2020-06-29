8bfc7c2d72a3cdff15a099b32ac92595a9618bbd
triplej <his1monologue@gmail.com>
2020-06-29 20:51:54 +0900

    컴포넌트 까지

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+import Mycomponent from './Mycomponent';
+import './App.css';
+
+function App() {
+  return (
+    <Mycomponent name="React" age={4} />
+  );
+}
+
+export default App;
