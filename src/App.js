5345c29970c6d0d859734dbae239e31ba44b1762
triplej <his1monologue@gmail.com>
2020-07-02 12:29:35 +0900

    6장 끝

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 import './App.css';
 
 class App extends Component {
   render(){
     return(
       <div>
-        <ScrollBox ref={(ref) =>this.ScrollBox=ref} />
-        <button onClick={() => this.ScrollBox.scrollToBottom()}>
-          맨밑으로
-        </button>
-
+        <IterationSample/>
       </div>
-          )
+      );
   }
 }
 
