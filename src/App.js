1f0faedac1293f341b7b1237faf56fe5cc02e5f4
triplej <his1monologue@gmail.com>
2020-07-02 10:28:59 +0900

    5장 끝

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import EventPractice from './EventPractice';
+import ScrollBox from './ScrollBox';
 import './App.css';
 
 class App extends Component {
   render(){
     return(
-      <EventPractice />
-    )
+      <div>
+        <ScrollBox ref={(ref) =>this.ScrollBox=ref} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>
+          맨밑으로
+        </button>
+
+      </div>
+          )
   }
 }
 
